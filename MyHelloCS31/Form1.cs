1293f566e540d41027834845508faebb1596d26d
Yujin Kim <a76363282@gmail.com>
2022-04-27 10:12:37 +0900

    #3 그럴싸한 환영 인사 동작 추가 환영 인사 메시지가 웃김

--- MyHelloCS31/Form1.cs
+++ MyHelloCS31/Form1.cs
@@ -19,7 +19,10 @@ namespace MyHelloCS31
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            MessageBox.Show("안녕하세요! 김유진입니다~",
+                "말풍선", // 창 제목
+                MessageBoxButtons.YesNo, // MessageBoxButtons의 옵션
+                MessageBoxIcon.Question);
         }
     }
 }
